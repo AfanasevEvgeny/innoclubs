7d65679412fde6a3febd1e08462dae2390c0902e
timurnugaev <decter49pro@gmail.com>
2020-09-21 16:14:36 +0300

    added simple server for test

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-// server
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+{
+    res.send('TESTUMBA!')
+});
+
+app.listen(port, () =>
+{
+    console.log(`Server listening at port: ${port}`)
+});
