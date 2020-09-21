0d6fe0e04cee3b94105f3e27a29cfefb400dff01
timurnugaev <decter49pro@gmail.com>
2020-09-21 18:00:11 +0300

    should hopefully work with heroku (added proc) attempt #3 restructured the project for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('../public'));
+app.use(express.static('public'));
 
 app.listen(port, () =>
 {
