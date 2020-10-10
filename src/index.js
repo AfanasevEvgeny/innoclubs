2ea1a460e75e20d6ffcb1b395a8a4ccb045f7051
Ziucay <justlumajustmail@gmail.com>
2020-10-10 17:22:36 +0300

    mongoDB initial setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/muggers-db");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
