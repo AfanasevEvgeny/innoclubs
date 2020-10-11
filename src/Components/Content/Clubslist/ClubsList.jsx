e720e1ae00f2cd16294cd07daeea22b9d8ba7c58
timurnugaev <decter49pro@gmail.com>
2020-10-12 00:35:40 +0300

    club info added

--- src/Components/Content/Clubslist/ClubsList.jsx
+++ src/Components/Content/Clubslist/ClubsList.jsx
@@ -5,9 +5,9 @@ const ClubsList =()=>{
     return(
         <div className={s.clubsList}>
             <div className={s.container}>
-                <div className={s.item}>club+description</div>
-                <div className={s.item}>club+description</div>
-                <div className={s.item}>club+description</div>
+                <div className={s.item}>SB Club <br> some description</div>
+                <div className={s.item}>Other club <br> description</div>
+                <div className={s.item}>Not interesting <br> no description</div>
                 <div className={s.item}>club+description</div>
                 <div className={s.item}>club+description</div>
                 <div className={s.item}>club+description</div>
