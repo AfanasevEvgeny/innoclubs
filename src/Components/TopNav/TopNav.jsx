6f834cb99e0651eabd3d41df0c162be306ba413b
Evgeny <66548875+AfanasevEvgeny@users.noreply.github.com>
2020-10-11 23:34:26 +0300

    setting up some components

--- src/Components/TopNav/TopNav.jsx
+++ src/Components/TopNav/TopNav.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import s from './TopNav.module.css'
 
-const TopNav =()=>{
-    return(
+
+const TopNav = () => {
+    return (
         <div className={s.topnav}>
-topnav
+            <div className={s.wrapper}>
+                <div className={s.item}>
+                    <div className={s.logo}>
+                        <h2>icon</h2>
+                    </div>
+                </div>
+                <div className={s.itemCenter}>
+                    <div className={s.headcontent}>
+                        <div className={s.searchBox}>
+
+                            <input type="text" className={s.myInput} onKeyUp="myFunction()" placeholder="Search for clubs.."/>
+                        </div>
+
+                    </div>
+                </div>
+                <div className={s.item}>
+                    <div className={s.account}>
+                        <h2>User</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
