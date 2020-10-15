b9007d4b02c5c1a00219d556ba69a86f84738a4f
timurnugaev <decter49pro@gmail.com>
2020-10-15 18:58:29 +0300

    added user club list + link with front added filtered (member-only/leader-only/any/my) club list fetching from database

--- public/js/main.js
+++ public/js/main.js
@@ -23,49 +23,117 @@ const db_root = firebase.database().ref();
 // db_root.child('clubs').child('sb club').child('description').set('new description');
 
 // User creds
-let user_name = "aladdinych"; // hardcode
-let user_inno_mail;
-let user_clubs; // club identificators of all the clubs of the user
-db_root.child('users').child('aladdinych').child('clubs').on('value', function (uclubs_snap)
+let USER_NAME = "aladdinych"; // hardcode
+update_my_clubs(); // get all the clubs (cuz no parameters call)
+update_my_clubs(USER_NAME, "member"); // get USER_NAME's clubs (member)
+update_my_clubs(USER_NAME, "leader"); // get USER_NAME's clubs (lead)
+update_my_clubs(USER_NAME); // get USER_NAME's clubs (lead)
+
+/**
+ * procedure that fetches Clubs data from Database and loads it into state
+ * => straight to react
+ *
+ * @param user_name
+ * if not undefined => returns list of all the clubs in the database
+ * if there's user_name => returns list of all the clubs for this user
+ *
+ * @param role
+ * if role = member => returns all the clubs for this user, where this user has member permissions
+ * if role = leader => returns all the clubs for this user, where this user has leader permissions
+ * if role = undef => returns all the clubs for this user, regardless of their role in these clubs
+ */
+function update_my_clubs(user_name, role) // role -> perm_mask in the future
 {
-    // Getting the list of all user's clubs from DB & parsing it into JS object
-    user_clubs = uclubs_snap.val();
-    user_clubs = user_clubs.split(', ');
-    console.log('user clubs', user_clubs);
-
-    // Traverse every of the user's clubs and request info from DB for each of them. Then show it to the user
-    let clubs_to_show = []; // temp array of clubs that we'll show to the user
-    for (let i = 0; i < user_clubs.length; i++)
+    if (user_name)
     {
-        // request to DB
-        db_root.child('clubs').child(user_clubs[i]).on('value', function (club_info_snap)
+        let user_clubs;
+        // Request user's clubs
+        db_root.child('users').child(user_name).child('clubs').on('value', function (uclubs_snap)
         {
-            // requested data for the i-th club
-            let club = club_info_snap.val();
-            console.log('info retrieved from db:', club);
+            // Getting the list of all user's clubs from DB & parsing it into JS object
+            user_clubs = uclubs_snap.val();
+            user_clubs = user_clubs.split(', ');
+            console.log('user clubs', user_clubs);
 
-            clubs_to_show.push({
-                id: i,
-                name: user_clubs[i],
-                brief_description: club.brief
-            });
-
-            if (i === user_clubs.length - 1)
+            // Traverse every of the user's clubs and request info from DB for each of them. Then show it to the user
+            let clubs_to_show = []; // temp array of clubs that we'll show to the user
+            for (let i = 0; i < user_clubs.length; i++)
             {
-                state.ClubsData = clubs_to_show;
-                console.log("new state clubs data", state.ClubsData);
+                // request to DB
+                db_root.child('clubs').child(user_clubs[i]).on('value', function (club_info_snap)
+                {
+                    // requested data for the i-th club
+                    let club = club_info_snap.val();
+                    console.log('info retrieved from db:', club);
 
+                    if (role === "leader" && club.owner === user_name ||
+                        role === "member" && club.owner !== user_name ||
+                        !role) // both member and leader - clubs
+                    {
+                        clubs_to_show.push({
+                            id: i,
+                            name: user_clubs[i],
+                            brief_description: club.brief
+                        });
+                    }
 
-                // ReactDOM.render(
-                //     <React.StrictMode>
-                //         <App AppState={state}/>
-                //     </React.StrictMode>,
-                //     document.getElementById('root')
-                // );
+                    if (i === user_clubs.length - 1)
+                    {
+                        if (!role)
+                        {
+                            state.MyClubsData = clubs_to_show;
+                        }
+                        else if (role === "member")
+                        {
+                            state.MyMemberClubsData = clubs_to_show;
+                        }
+                        else if (role === "leader")
+                        {
+                            state.MyLeaderClubsData = clubs_to_show;
+                        }
+                        window.can_render = true;
+                    }
+                });
             }
+            if (!role)
+            {
+                state.MyClubsData = clubs_to_show;
+            }
+            else if (role === "member")
+            {
+                state.MyMemberClubsData = clubs_to_show;
+            }
+            else if (role === "leader")
+            {
+                state.MyLeaderClubsData = clubs_to_show;
+            }
+            window.can_render = true;
         });
     }
+    else
+    {
+        let all_clubs;
+        // Request all the clubs
+        db_root.child('clubs').on('value', function (clubs_snap)
+        {
+            all_clubs = clubs_snap.val();
+            console.log('all clubs:', all_clubs);
 
-    // change visual: state.ClubsData = ...
-    console.log(state.ClubsData);
-});
+            let clubs_to_show = [];
+            let i = 0;
+            for (let club_name in all_clubs)
+            {
+                let club = all_clubs[club_name]; // current club
+                clubs_to_show.push({
+                    id: i++,
+                    name: club_name,
+                    brief_description: club.brief
+                });
+            }
+
+            state.ClubsData = clubs_to_show;
+            // that's when you render React
+            console.log('new club data', state.ClubsData);
+        });
+    }
+}
--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,9 +1,7 @@
 window.state = {
-    ClubsData: [
-        {id: 0, name: 'Innoclub', brief_description: 'lalallalalal'},
-        {id: 1, name: 'Innoclub2', brief_description: 'elellelel'},
-        {id: 2, name: 'Innoclub3', brief_description: 'edfsfdsdfs'},
-        {id: 3, name: 'Innoclub4', brief_description: 'gagagga'}
-    ]
+    ClubsData: [],
+    MyClubsData: [],
+    MyMemberClubsData: [],
+    MyLeaderClubsData: [],
 };
 export default window.state;
