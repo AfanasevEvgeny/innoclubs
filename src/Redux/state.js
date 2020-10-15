ae99928938dfef70d9533d9637a31d704523bcef
timurnugaev <decter49pro@gmail.com>
2020-10-15 15:41:27 +0300

    database link test

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,9 +1,9 @@
-let state = {
+window.state = {
     ClubsData: [
         {id: 0, name: 'Innoclub', brief_description: 'lalallalalal'},
         {id: 1, name: 'Innoclub2', brief_description: 'elellelel'},
         {id: 2, name: 'Innoclub3', brief_description: 'edfsfdsdfs'},
         {id: 3, name: 'Innoclub4', brief_description: 'gagagga'}
     ]
-}
-export default state;
+};
+export default window.state;
